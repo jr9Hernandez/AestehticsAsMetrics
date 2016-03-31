072f6bd85b3e224a103aa38075865624261d9857
Julian <richardes71@gmail.com>
2016-03-31 00:15:44 -0300

    working

--- AesteticsAsMetrics/src/br/ufv/dpi/metrics/ComputeMetrics.java
+++ AesteticsAsMetrics/src/br/ufv/dpi/metrics/ComputeMetrics.java
@@ -136,7 +136,7 @@ public class ComputeMetrics {
 		}*/
 		int i=0;
 		for(LabeledLevel level : labeledLevels) {
-
+			
 			System.out.println(level.getVisualAesthetics()+","+reacheabilityValues.get(i));
 			i++;			
 			
