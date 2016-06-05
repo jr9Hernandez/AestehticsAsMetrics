f75bf01c6b86e0cd577ecc90fd00dfbc8bc68881
Julian <richardes71@gmail.com>
2016-06-05 16:41:55 -0300

    working

--- AesteticsAsMetrics/src/br/ufv/dpi/metrics/Main.java
+++ AesteticsAsMetrics/src/br/ufv/dpi/metrics/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
 	public static void main(String args []) {
 		ComputeMetrics m = new ComputeMetrics();
-		m.compute();
-		m.printMetrics();
+		for(int i=5;i<30;i++)
+		{
+			m.compute();
+			m.printMetrics();
+		}
 		//m.printLabels();
 	}
 	
